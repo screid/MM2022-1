721b7664e961a6a51ccd16ee639f028ac53a95fa
ValeMagicSoul <101939635+ValeMagicSoul@users.noreply.github.com>
2022-03-19 16:49:16 +0000

    hola

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("a")
+#Problema 1:
+"""
+numero=4
+
+if numero % 2 ==0:
+  print ("sí es par")
+
+else :
+ 2 print("no es par")
+"""""
+
+
+#Pregunta 2:
+"""
+pregunta=input("Quieres pizza ")
+if pregunta =="si":
+  print("Toma tu pizza")
+elif pregunta =="no":
+  print("buuu")
+
+else:
+  print("no le entiendo")
+""" 
+
+
+#Problema 3:
+i=0
+suma=0
+while i<=10:
+  suma=suma+i
+  print (suma)
+  i=i+1
+print("suma total: ",suma)  
+
+
+#Problema 4:
+suma=0
+for i in range (0,11,1):
+  if i %2==0:
+   suma=suma+i
+   print (suma)
+
